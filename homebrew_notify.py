a73ff08c2c048a9b8fb698cde3c169026670f8d5
Jonathan Gopel <jonathan@josh.ai>
2019-07-26 16:22:01 -0600

    Teach script to check for outdated taps

--- homebrew_notify.py
+++ homebrew_notify.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 """Send OSX notification when a tap or cask update is available in Homebrew"""
 
+import collections
 import json
 import subprocess
+
+# Type for casks and taps that are outdated
+OutdatedFormuala = collections.namedtuple(typename="OutdatedFormual",
+                                          field_names="package installed_version current_version")
 
 
 def notify(*, text, title=None, subtitle=None):
@@ -20,3 +25,15 @@ def notify(*, text, title=None, subtitle=None):
 def brew_update():
     """Do the `brew update` command"""
     subprocess.check_call(["brew", "update"], stdout=subprocess.DEVNULL)
+
+
+def brew_outdated():
+    """Do the `brew outdated` command"""
+    outdated_taps = json.loads(subprocess.check_output(["brew", "outdated", "--json=v1"]))
+    return [
+        OutdatedFormuala(
+            package=tap["name"],
+            installed_version=tap["installed_versions"][-1],
+            current_version=tap["current_version"],
+        ) for tap in outdated_taps if not tap["pinned"]
+    ]
