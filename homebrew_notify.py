32d70e5ca94338781e62f916117885684b9aae2b
Jonathan Gopel <jonathan@josh.ai>
2019-07-26 16:22:35 -0600

    Teach script to check for outdated casks

--- homebrew_notify.py
+++ homebrew_notify.py
@@ -3,11 +3,15 @@
 
 import collections
 import json
+import re
 import subprocess
 
 # Type for casks and taps that are outdated
 OutdatedFormuala = collections.namedtuple(typename="OutdatedFormual",
                                           field_names="package installed_version current_version")
+
+# Regex for detecting cask versions
+CASK_VERSION_REGEX = re.compile(r"(?P<package>.+) \((?P<installed_version>.+)\) != (?P<current_version>.+)")
 
 
 def notify(*, text, title=None, subtitle=None):
@@ -37,3 +41,21 @@ def brew_outdated():
             current_version=tap["current_version"],
         ) for tap in outdated_taps if not tap["pinned"]
     ]
+
+
+def brew_cask_outdated():
+    """Do the `brew cask outdated` command"""
+    outdated_casks = subprocess.check_output(["brew", "cask", "outdated", "--verbose"],
+                                             stderr=subprocess.STDOUT).decode("utf8")
+    output = []
+    for line in outdated_casks.splitlines():
+        match = CASK_VERSION_REGEX.match(line)
+        if not match:
+            continue
+        output.append(
+            OutdatedFormuala(
+                package=match.group("package"),
+                installed_version=match.group("installed_version"),
+                current_version=match.group("current_version"),
+            ))
+    return output
