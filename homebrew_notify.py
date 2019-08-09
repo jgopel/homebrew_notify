6daade61e6f40e2dd66e6086fc1b1bf45f06e4e9
Jonathan Gopel <jonathan@josh.ai>
2019-08-09 10:33:23 -0600

    Add crontab installation
    I tried really hard to use python-crontab, but it just didn't seem
    capable of doing what I wanted, so I had to fall back to manual shell
    commands, which is gross but I couldn't find a better option.

--- homebrew_notify.py
+++ homebrew_notify.py
@@ -6,12 +6,31 @@ import json
 import re
 import subprocess
 
+from pathlib import Path
+
+INSTALL_DIR = ".homebrew_notify"
+
 # Type for casks and taps that are outdated
 OutdatedFormuala = collections.namedtuple(typename="OutdatedFormual",
                                           field_names="package installed_version current_version")
 
 # Regex for detecting cask versions
 CASK_VERSION_REGEX = re.compile(r"(?P<package>.+) \((?P<installed_version>.+)\) != (?P<current_version>.+)")
+
+
+def parse_args():
+    """Parse arguments"""
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Sends OSX terminal notification if brew has updates available")
+    parser.add_argument(
+        "--install",
+        help="Setup notifier to run automatically",
+        action="store_true",
+    )
+
+    args = parser.parse_args()
+    return args.install
 
 
 def notify(*, text, title=None, subtitle=None):
@@ -90,3 +109,39 @@ def notify_outdated_formula():
         subtitle=f"There are updates to {formulae_text}",
         text=" ".join(sorted([x.package for x in outdated_taps + outdated_casks])),
     )
+
+
+def install():
+    """Copy script to home directory and install it in the crontab"""
+    script_install_location = Path.home() / INSTALL_DIR / "homebrew_notify.py"
+
+    def copy_file():
+        import shutil
+        script_install_location.parent.mkdir(parents=True, exist_ok=True)
+        # TODO: Warn on an update once there's logging
+        shutil.copy(src=__file__, dst=script_install_location)
+
+    def setup_crontab():
+        current_crontab = subprocess.run(["crontab", "-l"], check=True, capture_output=True, text=True).stdout.strip()
+        if str(script_install_location) in current_crontab:
+            raise RuntimeError("Homebrew Notify already installed in crontab. Not updating crontab.")
+        crontab_line = f"*/30 * * * * PATH=/usr/local/bin:$PATH {script_install_location}"
+        new_crontab = "\n".join([current_crontab, crontab_line])
+        subprocess.run(["crontab", "-"], check=True, capture_output=True, text=True, input=new_crontab)
+
+    copy_file()
+    setup_crontab()
+
+
+def main():
+    """Run when script is run"""
+
+    do_install = parse_args()
+    if do_install:
+        install()
+    else:
+        notify_outdated_formula()
+
+
+if __name__ == "__main__":
+    main()
